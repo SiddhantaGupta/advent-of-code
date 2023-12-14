0eb8cfc765b3572c81b91ed71a57a1a9bf87b668
Siddhanta Gupta <rapstar.yosid007@gmail.com>
2023-12-14 15:34:07 +0530

    feat: 2023 day 14 part 2 solution

--- 2023/day14/main.py
+++ 2023/day14/main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import os
-from pprint import pprint
 
 
 def main():
@@ -12,8 +11,8 @@ def main():
     for i in range(0, len(input)):
         input[i] = list(input[i])
 
-    print(partOne(input))
-    # print(partTwo(input))
+    print("Part One: ", partOne(input))
+    print("Part Two: ", partTwo(input, 1000000000))
     return 0
 
 
@@ -22,6 +21,55 @@ def partOne(input):
     sm = shiftRocks(tm)
     w = calculateTotalRockWeight(sm)
     return w
+
+
+def partTwo(input, n):
+    cache = {}
+    hist = []
+    m = input
+    load = 0
+    for i in range(0, n):
+        for j in range(0, 4):
+            m = transposeMatrixRight(m)
+            m = shiftRocks(m)
+
+        key = ''.join(c for r in m for c in r)
+        if key in cache:
+            """ explaination for finding which calc in history is the ans: 
+            # complete rotation point in cycle = ((no. of times to rotate - rotations left of cycle) % cycle length) + rotations left of cycle
+            # index of ans in history = complete rotation point in cycle - 1
+
+            1. length of cycle = cycle end index - cycle start index
+
+            2. rotations before cycle window = cycle start index // due to 0 indexing of arrays
+
+            # rotations before cycle window will be added later
+            3. number of times to rotate after reaching cycle window = number of times to rotate - rotations before cycle window
+
+            # point in cycle window where final rotation will end
+            4. final rotation point in cycle = number of times to rotate after reaching cycle window % length of cycle
+
+            5. complete rotation point in sequence = final rotation point in cycle + rotations before cycle window
+
+            # 1 has been subracted in the below eq to find the index since arrays are 0 indexed
+            5. complete rotation index in sequence hist = complete rotation point in sequence - 1 """
+
+            cycleStartIndex = cache[key]
+            cycleEndIndex = i
+            cycleLength = cycleEndIndex - cycleStartIndex
+
+            rotationsLeftOfLoop = cycleStartIndex
+            rotationsInCycle = n - rotationsLeftOfLoop
+
+            lastRotationInCycleIndex = rotationsInCycle % cycleLength
+            fullRotationEndCycleIndex = lastRotationInCycleIndex + rotationsLeftOfLoop - 1
+
+            load = hist[fullRotationEndCycleIndex]
+            break
+        hist.append(calculateTotalRockWeight(transposeMatrixRight(m)))
+        cache[key] = i
+
+    return load
 
 
 def calculateTotalRockWeight(m):
